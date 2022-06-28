11cbc0ac42ff332f22d81f5cf10ac666bedef38f
SAI DUTT <duttganduri46@gmail.com>
2022-06-28 04:29:27 -0400

    added project by id route, clientInfo, project card components

---
+++ client/src/queries/projectQueries.js
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client';
+
+const GET_PROJECTS = gql`
+    query GetProjects {
+        projects {
+            id,
+            name,
+            status
+        }
+    }
+`
+
+const GET_PROJECT = gql`
+    query GetProject($id: ID!) {
+        project(id: $id) {
+            id
+            name
+            description
+            status
+            client {
+                id
+                name
+                email
+                phone
+            }
+        }
+    }
+
+`
+
+export { GET_PROJECTS, GET_PROJECT };
