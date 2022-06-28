b459f4f6cb83fb1d14e032525a1ce2ba9ddb4a33
SAI DUTT <duttganduri46@gmail.com>
2022-06-28 12:57:31 -0400

    heroku dep fix try 1

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true })
-      .then(() => console.log(`MongoDB Connected Successfully`.cyan.underline.bold))
-      .catch((err) => console.log(err));
-}
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
 
 module.exports = connectDB;
