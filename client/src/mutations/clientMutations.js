cf14d247ead5dc98de0d64e2a1ec96106e0be7af
SAI DUTT <duttganduri46@gmail.com>
2022-06-28 03:42:49 -0400

    add client func and completed client ui operations

--- client/src/mutations/clientMutations.js
+++ client/src/mutations/clientMutations.js
@@ -1,14 +1,26 @@
 import { gql } from '@apollo/client';
 
+const ADD_CLIENT = gql`
+    mutation addClient($name: String!, $email: String!, $phone: String!) {
+        addClient(name: $name, email: $email, phone: $phone) {
+            id
+            name
+            email
+            phone
+        }
+    }
+
+`;
+
 const DELETE_CLIENT = gql`
     mutation DeleteClient($id: ID!) {
         deleteClient(id: $id) {
-            id,
-            name,
-            email,
+            id
+            name
+            email
             phone
         }
     }
 `;
 
-export { DELETE_CLIENT };
+export { ADD_CLIENT, DELETE_CLIENT };
